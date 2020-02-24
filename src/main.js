26b70ec8885d94f1e92db73f7198a8bee92bf517
KenMwaura1 <kemwaura@gmail.com>
2020-02-24 14:42:50 +0300

    created the router folder

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
